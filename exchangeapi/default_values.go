2bd193c73483bbc0c08ecdd73030ebfe09790551
Volodymyr Burdeinyi <burdeinyi.v.o@gmail.com>
2021-07-20 10:25:21 +0300

    * enumeration replaced constant strings

--- exchangeapi/default_values.go
+++ exchangeapi/default_values.go
@@ -1,18 +1,19 @@
 package exchangeapi
 
-type CryptoExchange int
 const (
-	BinanceEx CryptoExchange = iota
-	Kucoin
+	BinanceEx = "binance"
+	KucoinEx  = "kucoin"
 )
 
 type OrderType int
+
 const (
 	Limit OrderType = iota
 	Market
 )
 
 type OrderSide int
+
 const (
 	Buy OrderSide = iota
 	Sell
