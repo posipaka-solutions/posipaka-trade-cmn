68a5e6ae3b95a92b00ae4a765d192a8ca95b7394
Volodymyr Burdeinyi <44869508+volvinbur1@users.noreply.github.com>
2021-08-14 17:36:48 +0300

    * name refactoring

--- exchangeapi/connector_structs.go
+++ exchangeapi/connector_structs.go
@@ -20,7 +20,7 @@ type ApiConnector interface {
 	GetOrdersList(assets symbol.Assets) ([]order.Info, error)
 	AddLimits(limits symbol.Limits)
 
-	BalancesInfo(quote string) (float64, error)
+	GetAssetBalance(asset string) (float64, error)
 
 	GetServerTime() (time.Time, error)
 }
